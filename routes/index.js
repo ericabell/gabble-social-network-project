36ea2a9c419c7285275e621447cfb58c27c7f824
Eric Abell <ericabell@users.noreply.github.com>
2017-09-03 12:28:27 -0500

    When logged in, display a list of gabs
    No formatting yet, just a simple ul

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,21 @@ const models = require('../models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.user);
-  if( req.user ) {
-    res.render('index', { title: 'Express', username: req.user.profile.displayName });
-  } else {
-    res.render('index', { title: 'Express' });
-  }
+
+  // Find all gabs in the messages table and display them appropriately
+  // if the user is logged in or logged out
+  models.message.findAll()
+    .then( (messages) => {
+      console.log(messages);
+      if( req.user ) {
+        res.render('index', { title: 'Express',
+                              username: req.user.profile.displayName,
+                              messages: messages
+                             });
+      } else {
+        res.render('index', { title: 'Express' });
+      }
+    })
 });
 
 router.get('/create', function(req, res, next) {
