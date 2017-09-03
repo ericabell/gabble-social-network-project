d13a957fbcb7d047dff2b8b09d1b429a39d0eb9e
Eric Abell <ericabell@users.noreply.github.com>
2017-09-03 10:58:19 -0500

    Route added for auth, Google auth functional

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+let session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
 
@@ -22,8 +26,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost:3000/auth/redirect"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(`In GoogleStrategy second arg:`);
+    console.log(accessToken, refreshToken, profile);
+
+    // here is where I would extract my app-specific info about this user
+    // and then pass it on in cb as an object.
+    // in this case, I'm passing all the info I get from profile on to serializeUser.
+
+    // this is my opportunity to take any information from the profile
+    // that I get from Google and pass it along to serializeUser where
+    // that information will be stored in session. If I don't do anything
+    // with the Google profile here, I won't get it back unless the user has
+    // to re-authenticate with Google.
+
+    // I can also store the accessToken or refreshToken somewhere if I need them
+    // to make later requests against the Google API.
+
+    return cb(null, {profile: profile['_json']})
+  }
+));
+
+// serializeUser will specify what data we want to be stored in session for
+// this authenticated user. It receives the user object from Google Strategy
+
+passport.serializeUser(function(user, done) {
+  console.log('in serializeUser, receives user from Google Strategy done');
+  console.log(`user:`);
+  console.dir(user);
+
+  // the call to done below tells my app that I want to store the object
+  // {profile. user.profile} in session.
+
+  done(null, {profile: user.profile});
+});
+
+// id gets passed whatever Express found in the session for the cookie sent
+// by the user. id is going to be assigned the object we gave as the second
+// argument to done at the end of serializeUser.
+passport.deserializeUser(function(id, done) {
+  console.log('in deserializeUser');
+  console.log(`id:`);
+  console.dir(id);
+  let err = null;
+
+  // I could also use the id passed to do some sort of local database lookup
+  // and get that data to place into req.user. That would save space in session.
+
+  // now this second argument is what gets placed in req.user by passport.
+  // I could place everything in req.user, but here I'm only putting the
+  // displayName and the link to a Google profile image for the user.
+  done(err, {displayName: id.profile.displayName, profileImage: id.profile.image.url});
+});
+
+app.use(session({ secret: 'key', // used to sign the session ID cookie
+                  resave: false, // forces session to be saved, even if it didn't change
+                  saveUninitialized: false, // forces an uninitialized session to be saved to the store
+                })
+        );
+
+app.use(passport.initialize());
+app.use(passport.session()); // be sure express-session is BEFORE passport-session
+
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+let passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+/* OAuth2 with Google */
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+// this is the redirect that Google calls once the user has passed Google Auth.
+router.get('/redirect',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+module.exports = router;
